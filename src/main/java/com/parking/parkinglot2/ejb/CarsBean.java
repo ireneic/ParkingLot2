de39b976b2367238d40185a838b97743b3b40a20
ireneic <ireneioana.costeiu@ulbsibiu.ro>
2025-11-24 19:09:36 +0200

    until 4.4

---
+++ src/main/java/com/parking/parkinglot2/ejb/CarsBean.java
@@ -0,0 +1,38 @@
+package com.parking.parkinglot2.ejb;
+
+import com.parking.parkinglot2.common.CarDto;
+import jakarta.ejb.EJBException;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.example.parkinglot.entities.Car;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Stateless
+public class CarsBean {
+
+    private static final Logger LOG = Logger.getLogger(CarsBean.class.getName());
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public List<CarDto> findAllCars() {
+        LOG.info("findAllCars");
+        try {
+            TypedQuery<Car> typedQuery =
+                    entityManager.createQuery("SELECT c FROM Car c", Car.class);
+            List<Car> cars = typedQuery.getResultList();
+            return copyCarsToDto(cars);
+        } catch (Exception ex) {
+            throw new EJBException(ex);
+        }
+    }
+
+    private List<CarDto> copyCarsToDto(List<Car> cars) {
+        return null;
+    }
+}
+
+
--- src/main/webapp/WEB-INF/pages/cars.jsp
+++ src/main/webapp/WEB-INF/pages/cars.jsp
@@ -1,5 +1,6 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <t:pageTemplate pageTitle="Cars">
     <h1>Welcome to the parking lot</h1>
